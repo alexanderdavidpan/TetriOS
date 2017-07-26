1b02d4c151a5195c05e0f62eac72a427c80a0444
Alex Pan <alexanderdavidpan@gmail.com>
2017-07-26 01:39:43 -0700

    Use swipeGestureRecognizer to implement quick shape drop functionality.
    - Only recognizes downwards swipes.
    - When downward swipe detected, dropShape function is called.

--- Tetrios/GameViewController.swift
+++ Tetrios/GameViewController.swift
@@ -81,7 +81,10 @@ class GameViewController: UIViewController, TetriosDelegate, UIGestureRecognizer
     }
     
     func gameShapeDidDrop(tetrios: Tetrios) {
-        
+        scene.stopTicking()
+        scene.redrawShape(tetrios.fallingShape!) {
+            tetrios.letShapeFall()
+        }
     }
     
     func gameShapeDidLand(tetrios: Tetrios) {
@@ -109,4 +112,26 @@ class GameViewController: UIViewController, TetriosDelegate, UIGestureRecognizer
             panPointReference = currentPoint
         }
     }
+    
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        tetrios.dropShape()
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UISwipeGestureRecognizer {
+            if otherGestureRecognizer is UIPanGestureRecognizer {
+                return true
+            }
+        } else if gestureRecognizer is UIPanGestureRecognizer {
+            if otherGestureRecognizer is UITapGestureRecognizer {
+                return true
+            }
+        }
+        return false
+    }
+    
 }
